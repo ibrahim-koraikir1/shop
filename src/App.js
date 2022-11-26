998d22e77d1ce03d786183f6e4b36dc614a3fe1a
ibraim <ikoraikir@gmail.com>
2022-11-26 11:25:14 -0800

    first commit

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
+import Home from './pages/Home';
+import ProductDetails from './pages/ProductDetails';
+
+
+
+const App = () => {
+  return (
+    <div className='overflow-hidden'>
+      <Router>
+        <Header />
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/product/:id' element={<ProductDetails />} />
+        </Routes>
+        <Sidebar />
+        <Footer />
+      </Router>
+    </div>
+  );
+};
+
+export default App;
---
+++ src/pages/ProductDetails.js
@@ -0,0 +1,17 @@
+import React, { useContext } from 'react';
+
+
+const ProductDetails = () => {
+ 
+  const { id } = useParams();
+
+  
+  
+  return (
+    <section className='pt-32 pb-12 lg:py-32 h-screen flex items-center'>
+      
+    </section>
+  );
+};
+
+export default ProductDetails;
