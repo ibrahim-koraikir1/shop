4d8e9412f23c9e8546be0315a07a3729f5e1b80b
ibraim <ikoraikir@gmail.com>
2022-11-30 06:42:13 -0800

    context/comp

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React from 'react';
 
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Footer from './components/Footer';
+import Header from './components/Header';
+import Sidebar from './components/Sidebar';
 
 import Home from './pages/Home';
 import ProductDetails from './pages/ProductDetails';
