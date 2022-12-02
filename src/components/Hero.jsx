053f526f4e6d090cd2f677652814fcaf99729da8
ibraim <ikoraikir@gmail.com>
2022-12-02 07:21:01 -0800

    fix hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,7 +1,38 @@
-import React from 'react'
+import React from 'react';
+// import images
+import WomanImg from '../img/woman_hero.png';
+// import link
+import { Link } from 'react-router-dom';
 
-export default function Hero() {
+const Hero = () => {
   return (
-    <div>Hero</div>
-  )
-}
+    <section className='w-full'>
+	<div className="bg-black text-white py-10">
+		<div className="container mx-auto flex flex-col md:flex-row items-center my-12 md:my-24">
+			<div className="flex flex-col w-auto mr-10 lg:w-1/3 justify-center items-start p-8">
+				<h1 className="text-3xl md:text-5xl p-2 text-yellow-300 tracking-loose">shoop</h1>
+				<h2 className="text-3xl md:text-5xl leading-relaxed md:leading-snug mb-2"> AUTUMN SALE STYLISH 
+				</h2>
+				<p className="text-sm md:text-base text-gray-50 mb-4">Explore your favourite events and
+					register now to showcase your talent and win exciting prizes.</p>
+				<a href="#"
+					className="bg-transparent hover:bg-yellow-300 text-yellow-300 hover:text-black rounded shadow hover:shadow-lg py-2 px-4 border border-yellow-300 hover:border-transparent">
+					Explore Now</a>
+			</div>
+			<div className="p-8 mt-10 mb-6 md:mb-0 md:mt-0 ml-3 md:ml-12 lg:w-2/3  justify-center w-full">
+				<div className="h-48 flex flex-wrap content-center">
+					<div>
+						<img className="inline-block mt-28 hidden xl:block" src="https://user-images.githubusercontent.com/54521023/116969935-c13d5b00-acd4-11eb-82b1-5ad2ff10fb76.png" alt='i'/></div>
+						<div>
+							<img className="inline-block mt-24 md:mt-0 p-8 md:p-0"  src="https://user-images.githubusercontent.com/54521023/116969931-bedb0100-acd4-11eb-99a9-ff5e0ee9f31f.png" alt='i'/></div>
+							<div>
+								<img className="inline-block mt-28 hidden lg:block" src="https://user-images.githubusercontent.com/54521023/116969939-c1d5f180-acd4-11eb-8ad4-9ab9143bdb50.png" alt='i'/></div>
+							</div>
+						</div>
+					</div>
+				</div>
+</section>
+  );
+};
+
+export default Hero;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,14 +1,14 @@
 import React, { useContext } from 'react';
-
+// import product context
 import { ProductContext } from '../contexts/ProductContext';
-
+// import components
 import Product from '../components/Product';
 import Hero from '../components/Hero';
 
 const Home = () => {
-  
+  // get products from product context
   const { products } = useContext(ProductContext);
- 
+  // get only men's & women's clothing category
   const filteredProducts = products.filter((item) => {
     return (
       item.category === "men's clothing" || item.category === "women's clothing"
